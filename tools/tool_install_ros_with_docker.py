88c04cd783c9bcfdc5025a5493de49f60a9f6ceb
LeiYang <2443638666@qq.com>
2023-05-03 02:04:06 +0800

    Update tool_install_ros_with_docker.py
    replace osrf with fishros while pull docker image

--- tools/tool_install_ros_with_docker.py
+++ tools/tool_install_ros_with_docker.py
@@ -17,11 +17,11 @@ class RosVersion:
 
 class RosVersions:
     ros_version = [
-        RosVersion('noetic',  'ROS1', RosVersion.STATUS_LTS, ['fishros2/ros:noetic-desktop-full'],["ros:noetic"]),
-        RosVersion('humble',  'ROS2', RosVersion.STATUS_LTS, ['fishros2/ros:humble-desktop-full'],["ros:humble"]),
-        RosVersion('foxy',  'ROS2', RosVersion.STATUS_LTS, ['fishros2/ros:foxy-desktop'],["ros:foxy"]),
+        RosVersion('noetic',  'ROS1', RosVersion.STATUS_LTS, ['osrf/ros:noetic-desktop-full'],["ros:noetic"]),
+        RosVersion('humble',  'ROS2', RosVersion.STATUS_LTS, ['osrf/ros:humble-desktop-full'],["ros:humble"]),
+        RosVersion('foxy',  'ROS2', RosVersion.STATUS_LTS, ['osrf/ros:foxy-desktop'],["ros:foxy"]),
         RosVersion('galactic',  'ROS2', RosVersion.STATUS_LTS, ['osrf/ros:galactic-desktop'],["ros:galactic"]),
-        RosVersion('melodic', 'ROS1', RosVersion.STATUS_LTS, ['fishros2/ros:melodic-desktop-full'],["ros:melodic"]),
+        RosVersion('melodic', 'ROS1', RosVersion.STATUS_LTS, ['osrf/ros:melodic-desktop-full'],["ros:melodic"]),
         RosVersion('rolling',  'ROS2', RosVersion.STATUS_LTS, ['osrf/ros:rolling-desktop-full'],["ros:rolling"]),
         RosVersion('kinetic', 'ROS1', RosVersion.STATUS_EOL, ['osrf/ros:kinetic-desktop-full'],["ros:kinetic"]),
         RosVersion('eloquent',  'ROS2', RosVersion.STATUS_EOL, ['osrf/ros:eloquent-desktop'],["ros:eloquent"]),
