772cb00fdb3344e1c98a0b24fc91d2935bf6d353
LeiYang <2443638666@qq.com>
2023-05-04 00:47:54 +0800

    Update tool_config_rosenv.py
    区别 docker 和 主机的 ～ 文件夹位置

--- tools/tool_config_rosenv.py
+++ tools/tool_config_rosenv.py
@@ -25,16 +25,12 @@ class Tool(BaseTool):
         # check and append source 
         result = CmdTask("ls /opt/ros/*/setup.bash", 0).run()
         is_docker_nv = CmdTask("ls /.dockerenv && echo 1 || echo 0 ", 0).run_all()
-        print(is_docker_nv[1][0])
-        print(is_docker_nv[1][0])
-        print(is_docker_nv[1][0])
-        print(is_docker_nv[1])
-        print(len(is_docker_nv[1]))
-        print(is_docker_nv[1])
-        if is_docker_nv[1] == ['0'] : print(2)
+        # is_docker_nv[0] 是 返回的成功与否的code
+        # is_docker_nv[1] 是 ls /.dockerenv 打印的信息 , 如果是docker 返回 /.dockerenv 和 1 如果不是 则 报错 （不计入is_docker_nv[1] ） 并返回 0
+        # [[/.dockerenv && echo 1 || echo 0 ]] 打印信息是 1或 0 但不是 is_docker_nv[1]的返回值， is_docker_nv[1]都是 0
         if is_docker_nv[1][0]=='0':  bashrc_result = CmdTask("ls /home/*/.bashrc", 0).run()
         if is_docker_nv[1][0]!='0':  bashrc_result = CmdTask("ls /root/.bashrc", 0).run() 
-        print(result[1])
+
         if len(result[1])>1:
             PrintUtils.print_info('检测到系统有多个ROS环境,已为你生成启动选择,修改~/.bashrc可关闭')
             data = get_source_command(result[1])
