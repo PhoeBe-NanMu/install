17beb3df8ed1a175b91e3902ffd155a8993b7c90
LeiYang <2443638666@qq.com>
2023-05-03 23:14:59 +0800

    Update tool_config_rosenv.py

--- tools/tool_config_rosenv.py
+++ tools/tool_config_rosenv.py
@@ -25,13 +25,9 @@ class Tool(BaseTool):
         # check and append source 
         result = CmdTask("ls /opt/ros/*/setup.bash", 0).run()
         is_docker_nv = CmdTask("ls -alh /.dockerenv", 0).run()
-        if is_docker_nv[0]!=0:  bashrc_result = CmdTask("ls /home/*/.bashrc", 0).run()
-            else:
-                bashrc_result = CmdTask("ls /root/.bashrc", 0).run() 
-                
-            
-        bashrc_result = CmdTask("ls ~/.bashrc", 0).run() 
-        if bashrc_result[0]!=0:  bashrc_result = CmdTask("ls /root/.bashrc", 0).run() 
+        if is_docker_nv[0]==0:  bashrc_result = CmdTask("ls /home/*/.bashrc", 0).run()
+        if is_docker_nv[0]!=0:  bashrc_result = CmdTask("ls /root/.bashrc", 0).run() 
+ 
         if len(result[1])>1:
             PrintUtils.print_info('检测到系统有多个ROS环境,已为你生成启动选择,修改~/.bashrc可关闭')
             data = get_source_command(result[1])
